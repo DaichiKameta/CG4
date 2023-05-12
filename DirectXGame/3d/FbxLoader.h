e548a7bcdff2bad9c4585838adaddf19e761c1fb
DaichiKameta <k021g1323@g.neec.ac.jp>
2023-05-12 10:03:16 +0900

    1-10

--- DirectXGame/3d/FbxLoader.h
+++ DirectXGame/3d/FbxLoader.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 
 #include "fbxsdk.h"
-
+#include "Model.h"
 #include <d3d12.h>
 #include <d3dx12.h>
 
@@ -16,7 +16,8 @@ private: // エイリアス
 public: //定数
 	//	モデル格納ルートパス
 	static const string baseDirectory;
-
+	// テクスチャがない場合の標準テクスチャファイル名
+	static const string defaultTextureFileName;
 public:
 	/// <summary>
 	/// シングルトンインスタンスの取得
@@ -36,10 +37,36 @@ public:
 	void Finalize();
 
 	/// <summary>
-	/// 
+	/// ファイルからFBXモデル読み込み
 	/// </summary>
 	/// <param name="modelName"></param>
 	void LoadModelFromFile(const string& modelName);
+
+	/// <summary>
+	/// 再帰的にノードを構成を解析
+	/// </summary>
+	/// <param name="model"></param>
+	/// <param name="fbxNode"></param>
+	void ParseNodeRecursive(Model* model, FbxNode* fbxNode, Node* parent = nullptr);
+
+	/// <summary>
+	/// メッシュ読み取り
+	/// </summary>
+	/// <param name="model"></param>
+	/// <param name="fbxNode"></param>
+	void ParseMesh(Model* model, FbxNode* fbxNode);
+
+	// 頂点座標読み取り
+	void ParseMeshVertices(Model* model, FbxMesh* fbxMesh);
+	// 面情報読み取り
+	void ParseMeshFaces(Model* model, FbxMesh* fbxMesh);
+	// マテリアル読み書き
+	void ParseMeshMaterial(Model* model, FbxNode* fbxNode);
+	// テクスチャ読み込み
+	void LoadTexture(Model* model, const std::string& fullpath);
+
+	// ディレクトリを含んだファイルパスからファイル名を抽出する
+	std::string ExtractFileName(const std::string& path);
 
 private:
 	// privateなコンストラクタ（シングルトンパターン）
